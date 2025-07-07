69bb044f08fbb6652f66bb828bf03ce31f30a402
leoonelc <cadenaleonel9@gmail.com>
2025-07-06 23:15:45 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using S6T1_MC;
+
+namespace INVENTARIOZAP
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new INV_ZAP());
+        }
+    }
+}
